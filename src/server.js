f131c37a2c36afcebd5fa5315ba0cc2200d6af70
Jose M. Silva <josemsilva1984@gmail.com>
2025-11-14 00:36:28 -0300

    Atualiza todos os arquivos e rotas

--- src/server.js
+++ src/server.js
@@ -8,18 +8,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const usuariosRoutes = require('./routes/usuarios'); // Importação de rotas de usuários
+
+// Caminhos ajustados para refletir a estrutura dentro de src/
+const usuariosRoutes = require('./routes/usuarios');
+const agendaRoutes = require('./routes/agenda');
 
 app.use(express.json());
 
-// Rota principal
 app.get('/', (req, res) => {
   res.send('Servidor em Operação');
 });
 
-
-// Conectando as rotas de usuários
 app.use('/usuarios', usuariosRoutes);
+app.use('/agenda', agendaRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
