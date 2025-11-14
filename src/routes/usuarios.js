2cedf4c866f6fab119c52c6ae86df3b0b3fa2e4c
Jose M. Silva <josemsilva1984@gmail.com>
2025-11-13 23:28:19 -0300

    Corrigida rota inicial /

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -8,6 +8,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Lista de usuários');
+});
+
+module.exports = router;
+
 // registrar usuário (Giovana ou Daniela)
 router.post('/registrar', (req, res) => {
   const { nome, email, senha } = req.body;
